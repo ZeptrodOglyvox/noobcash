f5a21b5432be708f839d647a538b6f2c6e724c80
Nikos Tsakas <ntsakas42@gmail.com>
2020-04-05 14:41:59 +0300

    Added an example batch file for running flask instances

---
+++ runserver(s).bat
@@ -0,0 +1,6 @@
+set FLASK_APP=backend
+set FLASK_ENV=development
+python -m flask run --host 127.0.0.1 --port 5000
+python -m flask run --host 127.0.0.1 --port 5001
+python -m flask run --host 127.0.0.1 --port 5002
+
